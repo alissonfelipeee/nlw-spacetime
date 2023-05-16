e32cbdf6e1630270f991d38fd70ecd938f972595
Alisson Felipe <alissonfelipenettt@gmail.com>
2023-05-16 20:19:46 -0300

    feat[web]: add empty memory list

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,6 +6,21 @@ export default function Home() {
 
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
       </div>
+
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a
+              href=""
+              className="underline transition-colors hover:text-gray-50"
+            >
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
     </main>
   )
 }
