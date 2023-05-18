62f5ec96654059c519364a3c5c348b58957241be
Alisson Felipe <alissonfelipenettt@gmail.com>
2023-05-18 20:46:19 -0300

    feat[server]: add file upload route

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,11 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 
-import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
 
@@ -12,12 +14,15 @@ app.register(cors, {
   origin: 'http://localhost:3000',
 })
 
+app.register(multipart)
+
 app.register(jwt, {
   secret: 'nlw-spacetime',
 })
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
