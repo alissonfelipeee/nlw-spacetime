ab74b4a617cdd75594cd4e0389382ab4dac8a79b
Alisson Felipe <alissonfelipenettt@gmail.com>
2023-05-18 21:01:18 -0300

    feat[server]: add exposure of files that have been uploaded

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,8 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import multipart from '@fastify/multipart'
+import fastifyStatic from '@fastify/static'
+import { resolve } from 'node:path'
 
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
@@ -12,6 +14,11 @@ const app = fastify()
 
 app.register(cors, {
   origin: 'http://localhost:3000',
+})
+
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '..', 'uploads'),
+  prefix: '/uploads',
 })
 
 app.register(multipart)
